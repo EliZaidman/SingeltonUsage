5b6b3d1e492ca89f016cf86d084287903f96fa59
Eli Zaidman <54203379+Rxelik@users.noreply.github.com>
2021-11-10 19:43:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingeltonToRead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.Instance.Increament();
+            Singleton.Instance.Increament();
+            Singleton.Instance.print();
+            Console.ReadLine();
+        }
+        //if it will create a new object then counter will be set to 1 again otherwise to 2.
+        //if new object will be created then it will print 0, otherwise it will print 2
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingeltonToRead
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance = null;
+        private static readonly object objLock = new object(); // Added new line
+        private int _counter = 0;
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (objLock)  // Added new block
+                {
+                    if (_instance == null)
+                        _instance = new Singleton();
+
+                    return _instance;
+                }
+            }
+        }
+
+        public int Increament()
+        {
+            return _counter++;
+        }
+
+        public void print()
+        {
+            Console.WriteLine("Counter Value: " + _counter.ToString());
+        }
+    }
+}
